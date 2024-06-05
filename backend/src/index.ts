5f7463c10ee768f900e04b2057fcbb594f9e7fb6
Melker Öberg <memleoberg@gmail.com>
2024-06-05 20:41:10 +0200

    render hosting

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ import userRoutes from './routes/users';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
